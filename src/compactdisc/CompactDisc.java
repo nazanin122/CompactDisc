4580e1ff25ea4a937e2bdf51b539e9d9387de75f
nazanin122 <nazanineliasy@gmail.com>
2024-12-01 23:53:24 -0500

    Update CompactDisc.java

--- src/compactdisc/CompactDisc.java
+++ src/compactdisc/CompactDisc.java
@@ -31,7 +31,6 @@ by reading from a file.
             cd[i] = new Song(title, artist); 
         }
         
-        // Print the contents of the array
         System.out.println("Contents of Classics:");
         for (int i = 0; i < cd.length; i++) {
             System.out.print(cd[i]); 
